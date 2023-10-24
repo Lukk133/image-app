ee117fc1f023be11495f5387798e7d70727124a0
Lukk133 <zielinskil@outlook.com>
2023-10-24 21:41:13 +0200

    Grid system

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,19 @@
 <template>
-<div></div>
+
+<VApp>
+  <SideBar></SideBar>
+  <VMain>
+    <ImageGallery/>
+  </VMain>
+</VApp>
+
 </template>
+
+<script setup>
+import ImageGallery from "./components/ImageGallery.vue";
+import SideBar from "./components/SideBar"
+ </script>
+
+
 
 
---
+++ src/components/SideBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-navigation-drawer expand-on-hover rail>
+    <v-list-item
+      prepend-avatar="https://qph.cf2.quoracdn.net/main-thumb-126103989-200-zznnaqrcesiwnhqtmpqnteoblncikoxe.jpeg"
+      title="Giga Chad" subtitle="Chad@gmail.com" nav>
+      <template v-slot:append>
+        <v-btn variant="text" icon="mdi-chevron-left" @click.stop="rail = !rail"></v-btn>
+      </template>
+    </v-list-item>
+    <v-divider />
+
+    <v-list densicty="compact" nav>
+      <v-list-item prepend-icon="mdi-image" title="Color Gallery" value="color" />
+    </v-list>
+
+    <v-list densicty="compact" nav>
+      <v-list-item prepend-icon="mdi-dialpad" title="Grayspace Gallery" value="color" />
+    </v-list>
+
+  </v-navigation-drawer>
+</template>
