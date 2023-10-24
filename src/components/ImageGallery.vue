12c63bc6f00a3da3ee16a1ad8e5a6cfe110b18ef
Lukk133 <zielinskil@outlook.com>
2023-10-24 21:46:54 +0200

    adding css

--- src/components/ImageGallery.vue
+++ src/components/ImageGallery.vue
@@ -1,38 +1,21 @@
 <template>
-  <v-row>
-    <v-col
-    v-for="n in 200"
-    cols="4"
-    sm="3"
-    md="2"
-    lg="1"
-    >
-      <v-card>
-        <v-img
-        :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
-        :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`"
-        aspect-ratio="1"
-        cover
-        class="bg-grey-lighten-2"
-        >
+  <v-card class="mx-5 my-2 pa-3">
+    <v-row>
+      <v-col v-for="n in 200" cols="4" sm="3" md="2" lg="1">
+        <v-card>
+          <v-img :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
+            :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`" aspect-ratio="1" cover class="bg-grey-lighten-2">
 
-        <template v-slot:placeholder>
-          <v-row
-          class="fill-height ma-0"
-          align="center"
-          justify="center"
+            <template v-slot:placeholder>
+              <v-row class="fill-height ma-0" align="center" justify="center">
+                <v-progress-circular indeterminate color="grey-lighten-5"></v-progress-circular><!--looding state-->
 
-          >
-            <v-progress-circular
-                indeterminate
-                color="grey-lighten-5"
-              ></v-progress-circular><!--looding state-->
-              
-          </v-row>
-        </template>
+              </v-row>
+            </template>
 
-        </v-img>
-      </v-card>
-    </v-col>
-  </v-row>
+          </v-img>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-card>
 </template>
